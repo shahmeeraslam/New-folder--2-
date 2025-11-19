d868c9ac8546ed506fc409e44128f6a09276a5d1
shahmeeraslam <shahmeeraslam976@gmail.com>
2025-11-19 06:38:36 -0800

    basic data types and casting, other basic topics are covered

--- first.py
+++ first.py
@@ -1,5 +1,26 @@
+# FIRST TASK 
+
 num1 = int(input("enter your first num : "))
 num2 = int(input("enter your second num : "))
 
 sum = num1 + num2
 print(sum)
+
+#  SECOND TASK 
+
+side = float(input("enter your side value : "))
+area = pow(side,2)
+print(area)
+
+# THIRD TASK
+
+FNUM1 = float(input("Enter your first floating value "))
+FNUM2 = float(input("Enter your second floating value "))
+Fsum = (FNUM1+FNUM2)/2
+print(Fsum)
+
+# fourth task 
+
+a = int(input("enter your first value :"))
+b = int(input("enter your second  value :"))
+print(a>=b)

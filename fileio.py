f57364ab422739987b09f7bbea9ba614af8fdd4d
shahmeeraslam <shahmeeraslam976@gmail.com>
2025-11-27 05:45:17 -0800

    file input and output functions

---
+++ fileio.py
@@ -0,0 +1,9 @@
+# testing file input and output funchtions and methods
+a= "hello world! \n"
+with open("hello.txt","a") as f:
+    f.write(a)
+
+
+with open("hello.txt" , "r") as r:
+    read =  r.read()
+    print(read)
